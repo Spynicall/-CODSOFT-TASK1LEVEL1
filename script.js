461fd16c4586ba85ad01e83b804ef776fd05e8dc
Spynicall <vibranium.mco@gmail.com>
2026-08-13 00:02:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-
 document.addEventListener('DOMContentLoaded', function() {
     var resumeBtn = document.getElementById('resumeBtn');
 
     if (resumeBtn) {
         resumeBtn.addEventListener('click', function() {
-        
         });
     }
 })
